96ea4aace2371c502e90172f43ee37c607c7e492
Andrico <andricominarrini@gmail.com>
2022-08-12 19:28:55 -0300

    Update Principal

--- AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
+++ AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
@@ -1,7 +1,7 @@
 package com.mycompany.autenticacaoconta;
 
 
-public class Principal {
+public class Aplicacao {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
