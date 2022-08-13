f19108a2ab100088ca4fdb81c9ff37e8b9b20da4
Andrico <andricominarrini@gmail.com>
2022-08-13 17:53:49 -0300

    fix

--- AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
+++ AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
@@ -11,13 +11,13 @@ public class Aplicacao {
 
 	public static void main( String[] args ) {
 
-		String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		/*String letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 		Random gerador = new Random();
 		String sms = new String();
 		for( int i = 0; i < 6; i++ ) {
 			sms = sms + ( letras.charAt( gerador.nextInt( letras.length() ) ) );
 		}
-		System.out.println( sms );
+		System.out.println( sms );*/
 
 		BancoSimulator dao = BancoSimulator.getInstancia();
                 Autorizacaofacade facade = new Autorizacaofacade();
