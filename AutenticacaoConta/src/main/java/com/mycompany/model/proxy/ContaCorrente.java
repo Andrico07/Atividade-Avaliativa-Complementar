d7b74ed9c9dd899e8d7d18957d855873805ee0d2
Andrico <andricominarrini@gmail.com>
2022-08-13 03:35:32 -0300

    Update ContaCorrente.java

--- AutenticacaoConta/src/main/java/com/mycompany/model/proxy/ContaCorrente.java
+++ AutenticacaoConta/src/main/java/com/mycompany/model/proxy/ContaCorrente.java
@@ -2,68 +2,91 @@ package com.mycompany.model.proxy;
 
 import com.mycompany.model.ContaCorrenteProxy;
 import com.mycompany.model.Usuario;
+import com.mycompany.model.chain.ProcessadorDeAutenticacao;
 
 
 public class ContaCorrente implements ContaCorrenteProxy {
-
-
+    
+    private Usuario usuario;
     private final ContaCorrenteReal contaCorrenteReal;
+    private ProcessadorDeAutenticacao processador;
 
     public ContaCorrente(Usuario usuario, String numero, double saldoInicial) {
+        this.usuario = usuario;
         contaCorrenteReal = new ContaCorrenteReal(usuario, numero, saldoInicial);
+        processador = new ProcessadorDeAutenticacao();
     }
     
     @Override
     public void depositar(double valor) {
-        contaCorrenteReal.depositar(valor);
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado())
+            contaCorrenteReal.depositar(valor);
     }
     
     @Override
     public void sacar(double valor) {
-        if(valor <= contaCorrenteReal.getSaldo())
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado() && valor <= contaCorrenteReal.getSaldo())
             contaCorrenteReal.sacar(valor);
     }
     
     @Override
     public void pagar(double valor) {
-        if(valor <= contaCorrenteReal.getSaldo())
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado() && valor <= contaCorrenteReal.getSaldo())
             contaCorrenteReal.pagar(valor);
     }
     
     @Override
     public void transferir(double valor, ContaCorrente contaDestino) {
-        if(valor <= contaCorrenteReal.getSaldo())
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado() && valor <= contaCorrenteReal.getSaldo())
             contaCorrenteReal.transferir(valor, contaDestino);
     }
     
     @Override
     public void ativar() {
-        contaCorrenteReal.ativar();
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado())
+            contaCorrenteReal.ativar();
     }
     
     @Override
     public void desativar() {
-        contaCorrenteReal.desativar();
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado())
+            contaCorrenteReal.desativar();
     }
 
     @Override
     public String getNumero() {
-        return contaCorrenteReal.getNumero();
+        processador.verificarAutenticacao(usuario);
+        //if(usuario.isAutenticado())
+            return contaCorrenteReal.getNumero();
     }
 
     @Override
     public double getSaldo() {
-        return contaCorrenteReal.getSaldo();
+        processador.verificarAutenticacao(usuario);
+        //if(usuario.isAutenticado())
+            return contaCorrenteReal.getSaldo();
     }
 
     @Override
     public boolean isAtiva() {
-        return contaCorrenteReal.isAtiva();
+        processador.verificarAutenticacao(usuario);
+        //if(usuario.isAutenticado())
+            return contaCorrenteReal.isAtiva();
     }
 
     @Override
     public Usuario getUsuario() {
-        return contaCorrenteReal.getUsuario();
+        processador.verificarAutenticacao(usuario);
+        if(usuario.isAutenticado())
+            return contaCorrenteReal.getUsuario();
+        else
+            return null;
     }
     
 }
