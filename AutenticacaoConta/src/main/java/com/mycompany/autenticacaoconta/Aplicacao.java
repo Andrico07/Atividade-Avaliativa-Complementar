fa8aa65a78669eef31e8f01c5e69af893a8e0925
Andrico <andricominarrini@gmail.com>
2022-08-13 03:36:51 -0300

    Update main

--- AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
+++ AutenticacaoConta/src/main/java/com/mycompany/autenticacaoconta/Aplicacao.java
@@ -1,9 +1,24 @@
 package com.mycompany.autenticacaoconta;
 
+import com.mycompany.model.Usuario;
+import com.mycompany.model.proxy.ContaCorrente;
+
 
 public class Aplicacao {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Usuario mario = new Usuario("mario", "cliente", "mario", "bros");
+        ContaCorrente conta1 = new ContaCorrente(mario, "1846", 10000);
+        
+        System.out.println(conta1.getSaldo());        
+        conta1.sacar(100);
+        System.out.println(conta1.getSaldo());
+        conta1.depositar(1000);
+        System.out.println(conta1.getSaldo());
+        conta1.sacar(10000);
+        System.out.println(conta1.getSaldo());
+        conta1.sacar(10000);
+        System.out.println(conta1.getSaldo());
     }
 }
