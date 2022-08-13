04e82b5822257876a27f8a5c3739a3a6a0c7a3a1
Mario <mario.junior@itixti.com.br>
2022-08-13 15:04:22 -0300

    Criação de banco

---
+++ AutenticacaoConta/src/main/java/DAO/BancoSimulator.java
@@ -0,0 +1,38 @@
+package DAO;
+
+import java.util.List;
+
+import com.mycompany.model.Usuario;
+
+public class BancoSimulator {
+
+	private List<Usuario> banco;
+
+	public void insert( Usuario usuario ) {
+		banco.add( usuario );
+	}
+
+	public boolean find( Usuario usuario ) {
+		for( Usuario user : banco ) {
+			if( user.equals( usuario ) ) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Usuario findNomeUsuarioSenha( String usuario, String senha ) {
+		for( Usuario user : banco ) {
+			if( user.getNomeUsuario().equals( usuario ) && user.getSenha().equals( senha ) ) {
+				return user;
+			}
+		}
+		return null;
+	}
+
+	public void delete( Usuario usuario ) {
+		if( find( usuario ) )
+			banco.remove( usuario );
+	}
+
+}
